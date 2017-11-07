a3d49ac409f4b97a307e703a33a2fe3ee85d7a26
Michael Smith <michael.smith@resonate.com>
2017-11-06 19:46:26 -0500

    commit work on thread caches

--- par_malloc.c
+++ par_malloc.c
@@ -1,6 +1,8 @@
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <syscall.h>
 
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -16,19 +18,34 @@
 
 #define ROUND_UP_TO_PAGE(x) ( (x + (PAGE_SIZE_BYTES - 1)) & (~(PAGE_SIZE_BYTES - 1)))
 
-#define SIZE_CLASS_0 16
-#define SIZE_CLASS_1 32
-#define SIZE_CLASS_2 64
-#define SIZE_CLASS_3 512
-#define SIZE_CLASS_4 1024
-#define SIZE_CLASS_5 2048
 
+#define SIZE_CLASS_0 32
+#define SIZE_CLASS_1 64
+#define SIZE_CLASS_2 128
+#define SIZE_CLASS_3 256
+#define SIZE_CLASS_4 512
+#define SIZE_CLASS_5 1024
+#define SIZE_CLASS_6 2048
+
+#define SIZE_CLASS_TO_SIZE(n) (1 << (5 + n))
+
+#define SIZE_CLASS_NUM 7
 #define THREAD_CACHE_ALLOC_MAX SIZE_CLASS_5
 
 
 typedef struct page_stack_node page_stack_node;
 struct page_stack_node {
     page_stack_node *next;
+};
+
+typedef struct bin_stack_node bin_stack_node;
+struct bin_stack_node {
+    bin_stack_node *next;
+};
+
+typedef struct thread_cache_bins thread_cache_bins;
+struct thread_cache_bins {
+    bin_stack_node *bins[SIZE_CLASS_NUM];
 };
 
 typedef struct block_header block_header;
@@ -44,11 +61,16 @@ const size_t BLOCK_HEADER_SIZE = sizeof(block_header);
 int _allocator_initialized = -1;
 pthread_once_t _init_once_control = PTHREAD_ONCE_INIT;
 
-// These are only used to lock the global free page stack which should ideally be minimally contested
-pthread_mutex_t global_mutex = PTHREAD_MUTEX_INITIALIZER;
+// This is only used to lock the global free page stack which should ideally be minimally contested
 pthread_spinlock_t global_spinlock;
 
+// Global page stack (needs to be locked)
 page_stack_node *global_page_stack = NULL;
+
+// Thread local thread cache (free list bins)
+__thread thread_cache_bins *thread_cache = NULL;
+__thread pid_t current_thread_id = 0;
+__thread int current_thread_initialized = 0;
 
 
 // Not thread safe
@@ -105,12 +127,68 @@ static void *get_free_page(void) {
 }
 
 
+// Initialize global allocator state
 static void xmalloc_init(void) {
-    pthread_mutex_lock(&global_mutex);
     (void) _alloc_system_pages_unsafe();
     pthread_spin_init(&global_spinlock, PTHREAD_PROCESS_PRIVATE);
     _allocator_initialized = 1;
-    pthread_mutex_unlock(&global_mutex);
+}
+
+
+static inline size_t best_size_class(size_t size) {
+    // TODO: Optimize this with bit tricks
+    if (size <= 32) {
+        return 0;
+    } else if (size <= 64) {
+        return 1;
+    } else if (size <= 128) {
+        return 2;
+    } else if (size <= 256) {
+        return 3;
+    } else if (size <= 512) {
+        return 4;
+    } else if (size <= 1024) {
+        return 5;
+    }
+    return 6;
+}
+
+
+static void replenish_thread_cache(void) {
+    for (size_t i = 0; i < SIZE_CLASS_NUM; i++) {
+        if (!thread_cache->bins[i]) {
+            void *page = get_free_page();
+            size_t bin_size = SIZE_CLASS_TO_SIZE(i);
+            size_t cells = PAGE_SIZE_BYTES / bin_size;
+            
+            char *current_cell = (char *) page;
+            for (size_t cell_num = 0; cell_num < cells - 1; cell_num++) {
+                bin_stack_node *node = (bin_stack_node *) current_cell;
+                node->next = (bin_stack_node *) (current_cell + bin_size);
+                current_cell = current_cell + bin_size;
+            }
+            ((bin_stack_node *) current_cell)->next = NULL;
+            thread_cache->bins[i] = (bin_stack_node *) page;
+        }
+    }
+}
+
+
+static inline void _init_thread_cache(void) {
+    thread_cache_bins *cache = get_free_page();
+    for (size_t i = 0; i < SIZE_CLASS_NUM; i++) {
+        cache->bins[i] = NULL;
+    }
+    thread_cache = cache;
+}
+
+
+// Initialize thread-local state
+static void xmalloc_thread_init(void) {
+    current_thread_id = syscall(SYS_gettid);
+    _init_thread_cache();
+    replenish_thread_cache();
+    current_thread_initialized = 1;
 }
 
 
@@ -120,7 +198,11 @@ static inline void _xmalloc_check_init(void) {
         pthread_once(&_init_once_control, xmalloc_init);
     }
     while (_allocator_initialized <= 0) {}
+    if (!current_thread_initialized) {
+        xmalloc_thread_init();
+    }
 }
+
 
 void *xmalloc(size_t size) { 
     _xmalloc_check_init();
